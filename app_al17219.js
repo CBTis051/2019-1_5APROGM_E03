0c8eb5820e1b3aff68ee857ef98193700e28b773
Ruy Jesé Luna Sandoval <al17219@cbtis051.edu.mx>
2019-11-27 12:09:51 -0700

    git

--- app_al17219.js
+++ app_al17219.js
@@ -1,15 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
-
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    var objeto = { 
-        mensaje : 'Hola mundo Node JS Repo GIT - Luna Sandoval Ruy Jesé\n' 
-    };
-
-    var json = JSON.stringify(objeto);
-
-    res.end(json);
-    
+    res.end('Hola mundo Node JS Repo GIT - Luna Sandoval Ruy Jesé\n');
 }).listen(8081);
 console.log('Server runing on port 8081');
