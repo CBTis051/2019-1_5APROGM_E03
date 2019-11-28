f80d0e9528931c5750e5804104f209221173a80d
Ontiveros Aguirre Jesús Vidal <al17250@cbtis051.edu.mx>
2019-11-27 19:03:46 -0700

    Commit inicial del server

---
+++ 2019-1_5APROGM_E03/app_al17219.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+
+    res.end('Hola mundo Node JS Repo GIT - Luna Sandoval Ruy Jesé\n');
+}).listen(8081);
+console.log('Server runing on port 8081');
